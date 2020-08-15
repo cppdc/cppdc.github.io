15607777b26c4ac3d546e85f7013066cbc812824
cppdc <69599527+cppdc@users.noreply.github.com>
2020-08-15 20:20:03 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+  $(".fairy-tail__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="img/prevarrow.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="img/nextarrow.svg" alt=""></button>',
+    autoplay: true,
+  });
+
+  $(".our-trip__slider").slick({
+    prevArrow:
+      '<button type="button" class="slick-btn slick-prev"><img src="img/prevarrow.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-btn slick-next"><img src="img/nextarrow.svg" alt=""></button>',
+    autoplay: true,
+  });
+});
